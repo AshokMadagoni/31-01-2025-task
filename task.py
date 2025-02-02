9d83c02f608fd669f68bb23c0bcaabe8166fccb6
AshokMadagoni <madagoniashok1@gmail.com>
2025-02-02 22:04:02 +0530

    updated

--- task.py
+++ task.py
@@ -1,38 +1,38 @@
 # # 1. Diagonal elements sum (Level 1 is any one diagonal and level 2 is )
 
-# # mat = [[1, 2, 3],
-# #        [4, 5, 6],
-# #        [7, 8, 9]]
 
-# # mat = [[1,  2,  3,  4],
-# #        [5,  6,  7,  8],
-# #        [9, 10, 11, 12],
-# #        [13,14, 15, 16]]
+# mat = [[1,  2,  3,  4],
+#        [5,  6,  7,  8],
+#        [9, 10, 11, 12],
+#        [13,14, 15, 16]]
 
-# # mat = [[1, 2],
-# #        [3, 4]]
+# mat = [[1, 2],
+#        [3, 4]]
 
 # mat = [[5]]
 
+mat = [[1, 2, 3],
+       [4, 5, 6],
+       [7, 8, 9]]
 
-# rowlen = len(mat)
-# collen = len(mat[0])
+rowlen = len(mat)
+collen = len(mat[0])
 
-# firstdiagonal = 0
-# seconddiagonal=0
+firstdiagonal = 0
+seconddiagonal=0
 
-# if rowlen == collen:
-#     k=rowlen-1
-#     for i in range(rowlen):  
-#         firstdiagonal += mat[i][i] 
-#         seconddiagonal+=mat[i][k]
-#         k-=1
-# totaldiagonal=firstdiagonal+seconddiagonal
-# if rowlen%2==1:
-#     totaldiagonal-=mat[rowlen//2][rowlen//2]
-# print(firstdiagonal)
-# print(seconddiagonal)
-# print(totaldiagonal)
+if rowlen == collen:
+    k=rowlen-1
+    for i in range(rowlen):  
+        firstdiagonal += mat[i][i] 
+        seconddiagonal+=mat[i][k]
+        k-=1
+totaldiagonal=firstdiagonal+seconddiagonal
+if rowlen%2==1:
+    totaldiagonal-=mat[rowlen//2][rowlen//2]
+print(firstdiagonal)
+print(seconddiagonal)
+print(totaldiagonal)
 
 
 # 2. Add 2 matrixes
